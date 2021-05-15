ec3c01977a4c94abe354d6534e9a9a8b94a9d1b3
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-15 19:32:16 +0900

    マイグレーション

--- db/migrate/20210515102949_create_ideas.rb
+++ db/migrate/20210515102949_create_ideas.rb
@@ -1,8 +1,8 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-      t.text :body
-
+      t.references :category, null: false, foreign_key: true
+      t.text :body, null: false
       t.timestamps
     end
   end
